34d140b6b1354f85e61d243e5df4802337df90f3
Vini <ovini.15515@gmail.com>
2025-11-06 17:24:50 -0300

    projeto

---
+++ Food Factory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Factory
+//
+//  Created by Vini Oliveira  on 06/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
