d764fb9e63462b8a010d299581aa7c13b1bfdf60
Vini <ovini.15515@gmail.com>
2025-11-12 17:40:01 -0300

    Delete Food Factory/Model/Service/ApiKey.swift

--- Food Factory/Model/Service/ApiKey.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ApiKey.swift
-//  Food Factory
-//
-//  Created by israel lacerda gomes santos on 12/11/25.
-//
-struct ApiKey {
-    static let value = "56af16672e0d46de8de72496065d4410"
-}
